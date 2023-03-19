72beb9648d9f63bacdb327b4df666d858456d33f
seoyoung7623 <seoyoung7623@naver.com>
2023-03-19 11:51:00 +0900

    2023.03.19 JPAtest

--- src/main/java/study/datajpa/repository/MemberJpaRepository.java
+++ src/main/java/study/datajpa/repository/MemberJpaRepository.java
@@ -5,6 +5,9 @@ import jakarta.persistence.PersistenceContext;
 import org.springframework.stereotype.Repository;
 import study.datajpa.entity.Member;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public class MemberJpaRepository {
     @PersistenceContext
@@ -15,8 +18,30 @@ public class MemberJpaRepository {
         return member;
     }
 
+    public void delete(Member member){
+        em.remove(member);
+
+    }
+
+    public List<Member> findAll(){
+        return em.createQuery("select m from Member m", Member.class)
+                .getResultList();
+        //전체조회, 특정 조건 query 조회 여기서 member는 entity/ Member.class는 반환타입
+    }
+
+    public Optional<Member> findById(Long id){
+        Member member = em.find(Member.class,id);
+        return Optional.ofNullable(member); //널일수도 있다.
+    }
+
+    public long count(){
+        return em.createQuery("select count(m) from Member m",Long.class)
+                .getSingleResult();//count는 long으로 반환
+
+    }
+
     public Member find(Long id){
-        return em.find(Member.class,id);
+        return em.find(Member.class,id); //하나 조회
     }
 }
 
--- src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
@@ -27,4 +27,19 @@ class MemberJpaRepositoryTest {
         assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
         assertThat(findMember).isEqualTo(member);
     }
+
+    @Test
+    public void basicCRUD(){
+        Member member1 = new Member("member1");
+        Member member2 = new Member("member2");
+        memberJpaRepository.save(member1);
+        memberJpaRepository.save(member2);
+
+        // 단건 조회 검증
+        Member findMember1 = memberJpaRepository.findById(member1.getId()).get();
+        Member findMember2 = memberJpaRepository.findById(member2.getId()).get();
+        assertThat(findMember1).isEqualTo(member1);
+        assertThat(findMember2).isEqualTo(member2);
+    }
+
 }
